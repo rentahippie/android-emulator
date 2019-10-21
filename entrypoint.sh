0d22a84998412809793b0b382c6774a810b1aa65
赵林 <zhaolin@zhaolins-Mac-mini.local>
2019-10-21 17:31:09 +0800

    update ip to 0.0.0.0

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,7 @@ fi
 /usr/sbin/sshd
 
 # Detect ip and forward ADB ports outside to outside interface
-ip=$(ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
+ip="0.0.0.0"
 socat tcp-listen:5037,bind=$ip,fork tcp:127.0.0.1:5037 &
 socat tcp-listen:5554,bind=$ip,fork tcp:127.0.0.1:5554 &
 socat tcp-listen:5555,bind=$ip,fork tcp:127.0.0.1:5555 &
